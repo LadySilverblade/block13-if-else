e8203258822fcfbb850346cf59c03f81d2c8509a
michelle <michelleladams83@gmail.com>
2023-04-21 16:07:30 -0700

    update to number line and greater than JS

---
+++ greater-than-5.js
@@ -0,0 +1,10 @@
+//Using if/else statements, JavaScript code in a script tag that prints true if both parameters are greater or equal to 5.
+const num1 = 10;
+const num2 = 11;
+if (num2 > num1) {
+	console.log('True');
+} else if (num1 === num2) {
+	console.log('True');
+} else if (num1 >= num2) {
+	console.log('False');
+}
